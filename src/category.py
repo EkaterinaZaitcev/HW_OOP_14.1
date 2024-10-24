79f7d6109b729154501936a6de872edd789c832a
Ekaterina <katyat1@mail.ru>
2024-10-24 14:44:46 +1000

    Прописаны классы Продукты и Категории, написаны тесты

--- src/category.py
+++ src/category.py
@@ -1,10 +1,26 @@
+from typing import List
+
+
 class Category:
+    """Класс для создания категории"""
     name: str
     description: str
-    products: str
+    products: List
+    category_count = 0
+    product_count = 0
+
 
     def __init__(self, name, description, products):
+        """Метод для инициализации категории"""
+        Category.category_count += 1
+        Category.product_count = len(products)
         self.name = name
         self.description = description
         self.products = products
+
+
+    def add_category(self):
+        """Добавление категорий"""
+        return f"Category(name={self.name}," f"description={self.description}," f"products={self.products})"
+
 
--- src/product.py
+++ src/product.py
@@ -1,15 +1,33 @@
 
 
 class Product:
+    """Класс для добавления продукта"""
     name: str
     description: str
     price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
-
+        """Метод для инициализации продукта"""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
+
+    def add_product(self):
+        """Добавление продукта"""
+        return (
+            f"Product(name={self.name}," 
+            f"description={self.description}," 
+            f"price={self.price}," 
+            f"quantity={self.quantity}"
+        )
+
+if __name__ == "__main__":
+    product_1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product_2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product_3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    print(product_1)
+    print(f"{product_2}\n")
+    print(f"{product_3}\n")
